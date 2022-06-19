9bc1d03f89fe4ef4d657e39ab0749e4f9081495d
Planetologist Topping <ptopping@at-g6.msk.21-school.ru>
2022-06-20 00:19:41 +0300

    my first commit: start working

---
+++ inc/philo.h
@@ -0,0 +1,14 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <sys/time.h> //gettimeofday 
+# include <pthread.h>
+
+//-lpthread
+
+
+#endif
